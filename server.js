4b9baefc0bae852c64a2bf31e8267ab14a2c3b63
Kelyanne Macedo <101842798+kelyannemacedo@users.noreply.github.com>
2025-12-16 20:50:07 -0300

    Modificação em user.js e auth.js para sincronizar dados 3 da api

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.use(
 /* =============================
    🔵 2. BODY PARSER (depois do CORS)
    ============================= */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+   app.use(express.json({ limit: '10mb' }))
+   app.use(express.urlencoded({ limit: '10mb', extended: true }))
+   
 
 /* =============================
    🔵 3. CONEXÃO COM MONGODB
